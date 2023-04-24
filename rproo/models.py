298629ef45b3a50300c0797b88c2462986eab0a5
akash <akashsen22@gmail.com>
2023-04-24 13:23:24 +0530

    frist
    Signed-off-by: akash <akashsen22@gmail.com>

--- rproo/models.py
+++ rproo/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Contact (models.Model):
     name = models.CharField(max_length=100,null=True,blank=True)
     email = models.EmailField( max_length=254,null=True,blank=True)
-    phone = models.PositiveIntegerField(max_length=12, blank=True,null=True, help_text='Contact phone number')
+    phone = models.PositiveIntegerField( blank=True,null=True, help_text='Contact phone number')
     message = models.TextField(null=True,blank=True)
 
     def __str__(self):
